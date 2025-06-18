3c2c0ec7ad44e9873c24b5ac519e34fe15009079
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2025-06-18 14:09:50 +0530

    deploy backend

--- stayfinder-backend/server.js
+++ stayfinder-backend/server.js
@@ -55,9 +55,4 @@ app.use("*", (req, res) => {
   res.status(404).json({ error: "Route not found" })
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
 module.exports = app;
